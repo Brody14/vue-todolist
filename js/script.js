289ccc3060eb7394cfab1502fec19a47a6c8e1b1
Brody14 <stefania.spinas@gmail.com>
2023-02-21 15:45:57 +0100

    FEAT delete task onclick

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
-console.log('VRSTY')
+//console.log("VRSTY");
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-        tasks: [
-          {
-            text: 'Togliere la muffa in bagno',
-            done: true,
-          },
-          {
-            text: 'Lavare i denti a chiwawa',
-            done: false,
-          },
-          {
-            text: 'Finire il bilancio 2022',
-            done: false,
-          },
-        ]
-      }
+createApp({
+	data() {
+		return {
+			tasks: [
+				{
+					text: "Togliere la muffa in bagno",
+					done: true,
+				},
+				{
+					text: "Lavare i denti a chiwawa",
+					done: false,
+				},
+				{
+					text: "Finire il bilancio 2022",
+					done: false,
+				},
+			],
+		};
+	},
+	methods: {
+    deleteTask(startIndex) {
+      //console.log(startIndex, this.tasks)
+      this.tasks.splice(startIndex, 1)
     }
-  }).mount('#app')
+  }
+}).mount("#app");
