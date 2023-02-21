e1a55e3107f613671871cb1d8a038eda1404b099
Brody14 <stefania.spinas@gmail.com>
2023-02-21 15:34:11 +0100

    FEAT object array

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-     
+        tasks: [
+          {
+            text: 'Togliere la muffa in bagno',
+            done: false,
+          },
+          {
+            text: 'Lavare i denti a chiwawa',
+            done: false,
+          },
+          {
+            text: 'Finire il bilancio 2022',
+            done: false,
+          },
+        ]
       }
     }
   }).mount('#app')
